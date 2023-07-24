771c860410122ef0adb6a1551a9a2a029a6884cc
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-24 13:11:07 +0100

    created helper functions to display availability in chairs/_card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+    def isPast?(datetime)
+        datetime < Time.now
+    end 
+
+    def display_availability(obj)
+        if obj.available_from.nil?
+            "Availability date not specified"
+        # if available, display "Available Now"
+        elsif (isPast?(obj.available_from))
+            "Available Now!"
+        # else, display "Available from [date]"
+        else  
+            "Available from #{obj.available_from.strftime('%Y-%m-%d')}"
+        end
+    end 
 end
