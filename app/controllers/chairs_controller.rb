364af0d1ca6b20b2eed33b6e81bb54535a5575c1
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-24 14:17:59 +0100

    fixed resources/linking

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -2,4 +2,8 @@ class ChairsController < ApplicationController
     def index 
         @chairs = Chair.all
     end
+
+    def show 
+        @chair = Chair.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "chairs#index"
+
+  resources :chairs
 end
