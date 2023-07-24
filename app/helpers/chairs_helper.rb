077c9aeea7f64d7608d38c83abba586e84c49f35
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-24 13:14:08 +0100

    moved chairs helper functions from application_helper to chairs_helper

--- app/helpers/chairs_helper.rb
+++ app/helpers/chairs_helper.rb
@@ -1,2 +1,16 @@
 module ChairsHelper
+
+    def isPast?(datetime)
+        datetime < Time.now
+    end 
+
+    def display_availability(chair)
+        if chair.available_from.nil?
+            "Availability not specified"
+        elsif (isPast?(chair.available_from))
+            "Available Now!"
+        else  
+            "Available from #{chair.available_from.strftime('%Y-%m-%d')}"
+        end
+    end 
 end
