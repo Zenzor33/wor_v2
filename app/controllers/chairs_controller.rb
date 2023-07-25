6026f1fe28b28e9eb2a7fd9c39b8da2b19e2dab8
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-25 15:45:04 +0100

    added update action to chairs controller

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -1,13 +1,22 @@
 class ChairsController < ApplicationController
     def index 
-        @chairs = Chair.all
+      @chairs = Chair.all
     end
 
     def show 
-        @chair = Chair.find(params[:id])
+      @chair = Chair.find(params[:id])
     end
 
     def edit 
-        @chair = Chair.find(params[:id])
+      @chair = Chair.find(params[:id])
+    end 
+
+    def update
+      @chair = Chair.find(params[:id])
+      chair_params = 
+				params.require(:chair).
+				  permit(:name, :price, :company, :picture, :description, :available_from)
+      @chair.update(chair_params)
+			redirect_to @chair
     end 
 end
