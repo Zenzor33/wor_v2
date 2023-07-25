998da8da67b3e770b8013080355f02fa46c2d664
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-25 13:00:58 +0100

    added form elements and changed form helper name to form_field_classes

--- app/helpers/chairs_helper.rb
+++ app/helpers/chairs_helper.rb
@@ -15,10 +15,11 @@ module ChairsHelper
     end 
 
     def label_classes 
+        # py-2 not tailwind
         "block text-sm font-medium leading-6 text-gray-900 py-2"
     end 
 
-    def text_field_classes
+    def form_field_classes
         "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
     end
 end
