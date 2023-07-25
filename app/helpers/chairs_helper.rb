78246a754a9daa91fba0c729806a72293e520818
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-25 12:53:27 +0100

    created helper methods in chairs_helper to store tailwind classes for form elements

--- app/helpers/chairs_helper.rb
+++ app/helpers/chairs_helper.rb
@@ -13,4 +13,12 @@ module ChairsHelper
             "Available from #{chair.available_from.strftime('%Y-%m-%d')}"
         end
     end 
+
+    def label_classes 
+        "block text-sm font-medium leading-6 text-gray-900 py-2"
+    end 
+
+    def text_field_classes
+        "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    end
 end
