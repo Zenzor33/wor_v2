d64168ab214c0a35efdb578beef8f82c606eea35
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-29 13:37:33 +0100

    converted form to simple_form

--- app/helpers/chairs_helper.rb
+++ app/helpers/chairs_helper.rb
@@ -23,8 +23,16 @@ module ChairsHelper
         "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
     end
 
+    def form_input_classes 
+        "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    end
+
     def form_field_with_error_classes
-        "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-red-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+        "mt-2 text-sm text-red-600"
+    end
+
+    def form_field_button_classes
+        "rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
     end
 
 end
