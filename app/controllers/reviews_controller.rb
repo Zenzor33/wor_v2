2bbdc1e4a3d160ff3da4bdbe446ba4a07962c46e
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-07-31 08:42:49 +0100

    created create controller and view for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,10 +9,24 @@ class ReviewsController < ApplicationController
   def new
     @review = @chair.reviews.new 
   end
+
+  def create 
+    @review = @chair.reviews.new(review_params)
+    if @review.save 
+      redirect_to chair_reviews_path(@chair), notice: "Review created successfully"
+    else  
+      redirect_to :new, status: :unprocessable_entity
+    end
+  end
 end
 
 private 
 
 def set_chair
   @chair = Chair.find(params[:chair_id])
+end
+
+def review_params
+  params.require(:review).
+   permit(:name, :stars, :comment)
 end
