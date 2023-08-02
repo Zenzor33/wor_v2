f0ff946ef07dd2005c4fde4c46e25e71c4bcd5bf
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-08-02 08:59:41 +0100

    refactoered reviews helper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,28 +1,17 @@
 module ReviewsHelper
 
-  def calculate_width(object, rating)
-    total_reviews_size = object.reviews.size
-    reviews_count = object.reviews.where(stars: rating).count
-    ratings_percentage = (reviews_count.to_f / total_reviews_size) * 100
+  def get_ratings_percentage(object, rating)
+    total_reviews_count = object.reviews.count
+    matching_ratings_count = object.reviews.where(stars: rating).count
+    ratings_percentage = matching_ratings_count.to_f / total_reviews_count * 100
+  end 
 
-    # "total reviews: #{total_reviews_size} reviews count: #{reviews_count} ratings percentage: #{ratings_percentage}"
-    if total_reviews_size.zero?
-      "width: 0%"
-    else 
-      "width: #{ratings_percentage}%"
-    end
+  def calculate_width(object, rating)
+    "width: #{get_ratings_percentage(object, rating).to_i}%"
   end 
 
   def reviews_percentage(object, rating)
-    total_reviews_size = object.reviews.size
-    reviews_count = object.reviews.where(stars: rating).count
-    ratings_percentage = reviews_count.to_f / total_reviews_size * 100
-    
-    if total_reviews_size.zero?
-      "0%"
-    else 
-      "#{ratings_percentage.to_i}%"
-    end
+    "#{get_ratings_percentage(object, rating).to_i}%"
   end 
 
 end
