635e386ff29ab9872071268fdd0cf4e6b9bf2b7a
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-08-03 14:29:50 +0100

    users create action implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: "User successfully created"
+    else  
+      render :new, status: :unprocessable_entity
+    end 
+  end
+
   def edit
     @user = User.find(params[:id])
   end 
 end
 
+private  
+
+def user_params
+  params.require(:user).
+   permit(:name, :email, :password)
+end
