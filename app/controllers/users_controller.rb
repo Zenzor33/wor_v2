9792833b8887677a306c75ce0fead3c6ec6c703d
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-08-04 19:15:33 +0100

    created user update, created password validations for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,11 +23,20 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end 
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User successfully updated"
+    else  
+      render :edit, status: :unprocessable_entity
+    end 
+  end
 end
 
 private  
 
 def user_params
   params.require(:user).
-   permit(:name, :email, :password)
+   permit(:name, :email, :password, :password_confirmation)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, 
+    presence: true,
     format: { with: /\S+@\S+/ },
     uniqueness: {case_sensitive: false}
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
