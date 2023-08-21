75c3bb19a56596bb19b545643a91bf15bd1abe33
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-08-21 22:48:49 +0100

    added scopes to chair model

--- app/models/chair.rb
+++ app/models/chair.rb
@@ -9,9 +9,12 @@ class Chair < ApplicationRecord
 
   validates :name, :available_from, presence: true
 
-  def self.available
-    where("available_from < ?", Time.now).order("available_from")
-  end
+  scope :available, -> { where("available_from < ?", Time.now).order("available_from") }
+  scope :low_price_chair, -> { where("price < ?", 500).order("price") }
+
+  # def self.available
+  #   where("available_from < ?", Time.now).order("available_from")
+  # end
 
   def average_rating
     reviews.average(:stars).round.to_i
