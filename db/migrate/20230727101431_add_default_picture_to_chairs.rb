291ee1141ba04ed0499fd5521c5c70b7e4c6216b
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-08-26 17:24:13 +0100

    convert jpeg to jpg

--- db/migrate/20230727101431_add_default_picture_to_chairs.rb
+++ db/migrate/20230727101431_add_default_picture_to_chairs.rb
@@ -1,5 +1,5 @@
 class AddDefaultPictureToChairs < ActiveRecord::Migration[7.0]
   def change
-    change_column :chairs, :picture, :string, default: "placeholder_chair.jpeg"
+    change_column :chairs, :picture, :string, default: "placeholder_chair.jpg"
   end
 end
--- db/migrate/20230803114841_add_default_avatar_to_users.rb
+++ db/migrate/20230803114841_add_default_avatar_to_users.rb
@@ -1,5 +1,5 @@
 class AddDefaultAvatarToUsers < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :avatar, :string, default: 'dhh.jpeg'
+    change_column :users, :avatar, :string, default: 'dhh.jpg'
   end
 end
