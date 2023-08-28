8ad2e85f29e52da925b826d849ecabc4084627c2
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-08-28 17:33:13 +0100

    added verified column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_095006) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_163131) do
   create_table "chairs", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_095006) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.boolean "verified", default: false
   end
 
   add_foreign_key "chairs", "users"
