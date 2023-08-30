a1b2e8384ba02fc6c8bb0cc6da53fa4887d0eccf
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2023-08-30 12:46:48 +0100

    created users helper method for avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def avatar(user)
+    if user.avatar.attached?
+      image_tag user.avatar
+        #.variant(resize_to_limit: [300, 300])
+    else
+      image_tag "dhh.jpg"
+    end
+  end
 end
